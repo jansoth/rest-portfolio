93136a470afcf6371f1365f55fe3992c01ccb5e9
JanBuechter <jan.buechter@posteo.de>
2025-02-22 20:43:28 +0100

    initial commit

---
+++ src/main/java/org/jan_buechter/rest_portfolio/RestPortfolioApplication.java
@@ -0,0 +1,13 @@
+package org.jan_buechter.rest_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jan_buechter/rest_portfolio/RestPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jan_buechter.rest_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
